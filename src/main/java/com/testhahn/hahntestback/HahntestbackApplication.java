5679d49d8c349bbb515a5660888b8b91d2b6fd08
pc <pc@DESKTOP-0LPHL7D>
2025-06-20 13:22:42 +0100

    feat / Initial commit and app configuration

---
+++ src/main/java/com/testhahn/hahntestback/HahntestbackApplication.java
@@ -0,0 +1,13 @@
+package com.testhahn.hahntestback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HahntestbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HahntestbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/testhahn/hahntestback/HahntestbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testhahn.hahntestback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HahntestbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
